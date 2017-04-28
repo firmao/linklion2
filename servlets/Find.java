b5c4bae521201d3599229844d2743ac5748c1ddd
Andre Valdestilhas <firmao@gmail.com>
2017-04-28 16:22:22 +0200

    Add files via upload

--- servlets/Find.java
+++ servlets/Find.java
@@ -39,8 +39,7 @@ public class Find extends HttpServlet {
 
 	private void handleRequestAndRespond(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			if(request.getParameter("uri") != null)
-			{
+			if(request.getParameter("uri") != null) {
 				String uri = request.getParameter("uri");
 				uri = uri.replaceAll("123nada", "#").trim(); // to solve some problems with QueryString.
 				request.getSession().setAttribute("uri", uri);
@@ -49,23 +48,82 @@ public class Find extends HttpServlet {
 
 				if((result != null) && (result.size() > 0))
 				{
-					//response.getOutputStream().println(result.toString());
+					response.getOutputStream().println("<table border='1'> "
+							+ "<tr> "
+							+ "<th>EndPoint</th> "
+							+ "<th>Count DataType</th> "
+							+ "</tr>");
 					result.entrySet().forEach(elem ->{
 						String endPoint = elem.getKey();
 						int dType = elem.getValue();
 						try {
-							response.getOutputStream().println(endPoint + " | " + dType);
+							response.getOutputStream().println("<tr> "
+									+ "<td>"+ endPoint +"</td> "
+									+ "<td>"+ dType +"</td> "
+									+ "</tr>");
 						} catch (IOException e) {
 							e.printStackTrace();
 						}
 					});
+					response.getOutputStream().println("</table>");
 				}else{
-					response.getOutputStream().println("<h1>NOTHING !<h1>");
+					response.getOutputStream().println("<h1>NOTHING !</h1>");
 				}
 				//response.getWriter().write(sameas.getSameAsURI(request.getParameter("uri"),true));
-			}	
+			}else if(request.getParameter("endpoint") != null) {	
+				findURIS(request, response);
+			}else if(request.getParameter("SQL") != null) {	
+				findSQL(request, response);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+
+	private void findSQL(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.getOutputStream().println("<h1>Not implemented yet</h1>");
+		
+	}
+
+	private void findURIS(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		//response.getOutputStream().println("<h1>Not implemented yet</h1>");
+		String endpoint = request.getParameter("endpoint");
+		endpoint = endpoint.replaceAll("123nada", "#").trim(); // to solve some problems with QueryString.
+		request.getSession().setAttribute("endpoint", endpoint);
+		
+		Map<String, Integer> result = DBUtil.findAllURIDataTypes(endpoint);
+
+		if((result != null) && (result.size() > 0))
+		{
+			String json = "[";
+			for (Map.Entry<String, Integer> elem : result.entrySet()) {
+				String uri = elem.getKey();
+				int dType = elem.getValue();
+				json += ",{ \"uri\":\""+ uri + "\",\"CountDataType\":\""+ dType + "\"}";
+			}
+			json = json.replaceFirst(",", "");
+			json += "]";
+			response.getOutputStream().println(json);
+//			response.getOutputStream().println("<table border='1'> "
+//					+ "<tr> "
+//					+ "<th>URI</th> "
+//					+ "<th>Count DataType</th> "
+//					+ "</tr>");
+//			result.entrySet().forEach(elem ->{
+//				String uri = elem.getKey();
+//				int dType = elem.getValue();
+//				try {
+//					response.getOutputStream().println("<tr> "
+//							+ "<td>"+ uri +"</td> "
+//							+ "<td>"+ dType +"</td> "
+//							+ "</tr>");
+//				} catch (IOException e) {
+//					e.printStackTrace();
+//				}
+//			});
+//			response.getOutputStream().println("</table>");
+		}else{
+			response.getOutputStream().println("<h1>NOTHING !</h1>");
 		}
 	}
 
