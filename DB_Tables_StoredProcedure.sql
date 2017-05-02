57bb4050887e83197a9fb840b485198ceee6b2a7
Andre Valdestilhas <firmao@gmail.com>
2017-05-03 00:07:05 +0200

    Update procedure now using LAST_INSERT_ID()
    Now using the function LAST_INSERT_ID() due to concurrency issues.

--- DB_Tables_StoredProcedure.sql
+++ DB_Tables_StoredProcedure.sql
@@ -15,33 +15,31 @@ CREATE TABLE linklion2.uri2 (
 DELIMITER //
 CREATE DEFINER=`root`@`localhost` PROCEDURE `ADD_DB`(IN _subj varchar(900), _datasetEndPoint varchar(900))
 BEGIN
-	DECLARE indexDS int(11) DEFAULT 0;
-	DECLARE cDtypes int(11) DEFAULT 0;
-    /*Select 'Starting... First Select'; */
-	Select indDS INTO indexDS from linklion2.dataset2 where name = _datasetEndPoint;	
-	/* select concat('indexDS = ', indexDS); */
-    IF (indexDS < 1) Then
-	Begin
-		/* select concat('Inserting dataset: ', indexDS); */
-		insert into linklion2.dataset2(name) values (_datasetEndPoint);	
-		COMMIT;
-	end;
-	End if;
-	Select max(indDS) INTO indexDS from linklion2.dataset2;
-	Select sum(countDType) into cDtypes from linklion2.uri2 where 
-		uri = _subj and indexDataset = indexDS;
-	
-    if(cDtypes IS NOT NULL) then
-    Begin
-		/* select (cDtypes + 1) into cDtypes; */
-		Update linklion2.uri2 set countDType = (cDtypes + 1) 
-        where uri = _subj and indexDataset = indexDS;
-	End;
-    else
-    Begin
-		Insert into linklion2.uri2(uri,indexDataset,countDType) values (_subj, indexDS, 1);
-    End;
-    End if;
-    COMMIT;
+DECLARE indexDS int(11) DEFAULT 0;
+DECLARE cDtypes int(11) DEFAULT 0;
+    
+Select indDS INTO indexDS from linklion2.dataset2 where name = _datasetEndPoint;
+
+IF (indexDS < 1) Then
+Begin
+	insert into linklion2.dataset2(name) values (_datasetEndPoint);
+	COMMIT;
+    set indexDS = LAST_INSERT_ID();
+end;
+End if;
+#Select max(indDS) INTO indexDS from linklion2.dataset2;
+Select sum(countDType) into cDtypes from linklion2.uri2 where 
+uri = _subj and indexDataset = indexDS;
+if(cDtypes IS NOT NULL) then
+Begin
+	Update linklion2.uri2 set countDType = (cDtypes + 1) 
+		where uri = _subj and indexDataset = indexDS;
+End;
+else
+Begin
+	Insert into linklion2.uri2(uri,indexDataset,countDType) values (_subj, indexDS, 1);
+End;
+End if;
+COMMIT;
 END
 //
