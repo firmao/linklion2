592033ce3d67a7fe90782b70d5203fde933899c4
Andre Valdestilhas <firmao@gmail.com>
2017-05-03 15:35:41 +0200

    new Stored Procedure

--- DB_Tables_StoredProcedure.sql
+++ DB_Tables_StoredProcedure.sql
@@ -43,3 +43,26 @@ End if;
 COMMIT;
 END
 //
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `ADD_DB_S`(IN _subj varchar(900), _datasetEndPoint varchar(900), cDTypes int(11))
+BEGIN
+DECLARE indexDS int(11) DEFAULT 0;
+    
+Select indDS INTO indexDS from linklion2.dataset2 where name = _datasetEndPoint;
+
+IF (indexDS < 1) Then
+Begin
+	insert into linklion2.dataset2(name) values (_datasetEndPoint);
+	COMMIT;
+    set indexDS = LAST_INSERT_ID();
+end;
+End if;
+#Select max(indDS) INTO indexDS from linklion2.dataset2;
+if(cDtypes IS NOT NULL) then
+Begin
+	Insert into linklion2.uri2(uri,indexDataset,countDType) values (_subj, indexDS, cDTypes);
+End;
+End if;
+COMMIT;
+END
+//
